3b0d560d06eaab6c6ab971f8d419e87cd995811a
Ege <egekolatan114@gmail.com>
2026-07-11 10:59:14 +0300

    İlk commit

---
+++ Frontend/HTML/ders14/script.js
@@ -0,0 +1,23 @@
+// //bu bir yorum satırıdır 
+// const a=90
+// console.log(a)
+// // let isim="ege"
+// console.log(isim)
+
+// değişken tanımlarken dolar altçizgi veya yazıyla başlamak zorundadır
+// 3 tip değişken tanımlama şekli vardır
+// 1 let modern js te genellikle kullanılan tanımlama türüdür
+// 2 var eskidir günümüzde çok kullanılmaz
+// 3 const içeriği değiştirilemez sabit veriler için kullanılır
+
+// let Isim="deneme"
+// console.log(ISım)
+// js büyük küçük harfe duyarlıdır
+
+// console.log("Hello World") // console.log consolda çıkıtları verir
+// alert("Hello World") // uyarı gibi sayfa ilk yüklendiğinde verir
+// document.write("Hello World")
+let isim=prompt("lütfen bir isim giriniz")
+let soyisim=prompt("lütfen soyisim giriniz")
+document.write(isim +" "+soyisim)
+alert("Merhaba "+isim+ " "+soyisim+" Hoşgeldiniz")
